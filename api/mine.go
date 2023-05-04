f21f0e05f296e66f03c1a6ae40a80c86570518dd
Frédéric Thomas <frederic.thomas2@icloud.com>
2023-05-04 09:19:28 +0200

    feat(mine): get best algo on startup

--- api/mine.go
+++ api/mine.go
@@ -10,10 +10,18 @@ import (
 	"text/template"
 
 	"github.com/gin-gonic/gin"
+	"github.com/squarefactory/miner-api/autoswitch"
 )
 
 func MineStart(c *gin.Context) {
 
+	// get algo and pool
+	algo := Algo{}
+	bestAlgo := autoswitch.GetBestAlgo(c)
+	algo.Algo = bestAlgo
+	algo.Pool = bestAlgo + ".auto.nicehash.com:443"
+
+	// get wallet id
 	data, err := c.GetRawData()
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error})
@@ -29,6 +37,10 @@ func MineStart(c *gin.Context) {
 	tmpl := template.Must(template.New("jobTemplate").Parse(JobTemplate))
 	var jobScript bytes.Buffer
 	if err := tmpl.Execute(&jobScript, wallet); err != nil {
+		c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+		return
+	}
+	if err := tmpl.Execute(&jobScript, algo); err != nil {
 		c.JSON(http.StatusOK, gin.H{"error": err.Error()})
 		return
 	}
